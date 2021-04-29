3f837ecc8118cf5fda2c15a7dedcf1d327b35e2d
DanSnow <dododavid006@gmail.com>
2021-04-29 10:39:30 +0800

    feat: initial commit

---
+++ src/FontPreview.js
@@ -0,0 +1,54 @@
+import '@storipress/fonts/fonts.css'
+
+import { fontList } from '@storipress/fonts/font-list.ts'
+import { Fragment, useState } from 'react'
+
+function readQuery() {
+  const params = new URLSearchParams(window.location.search)
+  return params.get('category')
+}
+
+export function FontPreview() {
+  const [text, setText] = useState('Hello world')
+  const filter = readQuery()
+
+  return (
+    <>
+      <label>
+        Preview Text:
+        <input
+          style={{ marginLeft: '1rem', border: 'blue 1px solid', paddingLeft: '0.5rem' }}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </label>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Preview</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(fontList).map((category) =>
+            filter == null || category.toLowerCase() === filter ? (
+              <Fragment key={category}>
+                {!filter ? (
+                  <tr>
+                    <th colSpan={2}>{category}</th>
+                  </tr>
+                ) : null}
+                {fontList[category].map((name) => (
+                  <tr key={name}>
+                    <td>{name}</td>
+                    <td style={{ fontFamily: name, fontSize: '2rem' }}>{text || 'Hello world'}</td>
+                  </tr>
+                ))}
+              </Fragment>
+            ) : null
+          )}
+        </tbody>
+      </table>
+    </>
+  )
+}
